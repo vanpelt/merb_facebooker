6c602a85e292dbf00a43037886676ee65d9a32ae
Chris Van Pelt <vanpelt@SnowWhite.local>
2008-03-29 19:48:36 -0700

    Initial checkin

---
+++ Rakefile
@@ -0,0 +1,45 @@
+require 'rubygems'
+require 'rake/gempackagetask'
+
+PLUGIN = "merb_facebooker"
+NAME = "merb_facebooker"
+VERSION = "0.0.1"
+AUTHOR = "Chris Van Pelt"
+EMAIL = "vanpelt@doloreslabs.com"
+HOMEPAGE = "http://merb-plugins.rubyforge.org/merb_facebooker/"
+SUMMARY = "Merb plugin that makes rfacebooker work with merb..."
+
+spec = Gem::Specification.new do |s|
+  s.name = NAME
+  s.version = VERSION
+  s.platform = Gem::Platform::RUBY
+  s.has_rdoc = true
+  s.extra_rdoc_files = ["README", "LICENSE", 'TODO']
+  s.summary = SUMMARY
+  s.description = s.summary
+  s.author = AUTHOR
+  s.email = EMAIL
+  s.homepage = HOMEPAGE
+  s.add_dependency('merb-core', '>= 0.9.0')
+  s.add_dependency('facebooker')
+  s.require_path = 'lib'
+  s.autorequire = PLUGIN
+  s.files = %w(LICENSE README Rakefile TODO) + Dir.glob("{lib,specs}/**/*")
+end
+
+Rake::GemPackageTask.new(spec) do |pkg|
+  pkg.gem_spec = spec
+end
+
+task :install => [:package] do
+  sh %{sudo gem install pkg/#{NAME}-#{VERSION}}
+end
+
+namespace :jruby do
+
+  desc "Run :package and install the resulting .gem with jruby"
+  task :install => :package do
+    sh %{#{SUDO} jruby -S gem install pkg/#{NAME}-#{Merb::VERSION}.gem --no-rdoc --no-ri}
+  end
+  
+end
---
+++ lib/merb_facebooker/merbtasks.rb
@@ -0,0 +1,49 @@
+namespace :facebooker do
+  desc "Create a basic facebooker.yml configuration file"
+  task :setup => :environment do   
+    facebook_config = File.join(RAILS_ROOT,"config","facebooker.yml")
+    unless File.exist?(facebook_config)
+      cp File.join(RAILS_ROOT,"facebooker.yml.tpl"), facebook_config 
+      puts "Configuration created in #{RAILS_ROOT}/config/facebooker.yml"
+    else
+      puts "#{RAILS_ROOT}/config/facebooker.yml already exists"
+    end
+  end
+  
+  namespace :tunnel do 
+    # Courtesy of Christopher Haupt
+    # http://www.BuildingWebApps.com
+    # http://www.LearningRails.com
+    desc "Create a reverse ssh tunnel from a public server to a private development server." 
+    task :start => [ :config ] do  
+      puts "Starting tunnel #{@public_host}:#{@public_port} to 0.0.0.0:#{@local_port}" 
+      exec "ssh -nNT -g -R *:#{@public_port}:0.0.0.0:#{@local_port} #{@public_host_username}@#{@public_host}" 
+    end 
+
+    desc "Create a reverse ssh tunnel in the background. Requires ssh keys to be setup." 
+    task :background_start => [ :config ] do  
+      puts "Starting tunnel #{@public_host}:#{@public_port} to 0.0.0.0:#{@local_port}" 
+      exec "ssh -nNT -g -R *:#{@public_port}:0.0.0.0:#{@local_port} #{@public_host_username}@#{@public_host} > /dev/null 2>&1 &" 
+    end 
+    
+    # Adapted from Evan Weaver: http://blog.evanweaver.com/articles/2007/07/13/developing-a-facebook-app-locally/ 
+    desc "Check if reverse tunnel is running"
+    task :status => [ :config ] do
+     if `ssh #{@public_host} -l #{@public_host_username} netstat -an | 
+         egrep "tcp.*:#{@public_port}.*LISTEN" | wc`.to_i > 0
+       puts "Seems ok"
+     else
+       puts "Down"
+     end
+    end
+
+    task :config do
+     facebook_config = File.dirname(__FILE__) + '/../../../../../config/facebooker.yml'
+     FACEBOOKER = YAML.load_file(facebook_config)[Merb.environment]
+     @public_host_username = FACEBOOKER['tunnel']['public_host_username'] 
+     @public_host = FACEBOOKER['tunnel']['public_host'] 
+     @public_port = FACEBOOKER['tunnel']['public_port'] 
+     @local_port = FACEBOOKER['tunnel']['local_port'] 
+    end
+  end
+end
